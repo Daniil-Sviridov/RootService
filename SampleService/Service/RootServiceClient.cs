d8fb027815d22ba39e7c128b9a4575dd0085ff58
Daniil Sviridov <daniil_sviridov@mail.ru>
2022-12-26 20:50:21 +0300

    st 3

--- SampleService/Service/RootServiceClient.cs
+++ SampleService/Service/RootServiceClient.cs
@@ -1,6 +1,28 @@
-﻿namespace SampleService.Service
+﻿using RootServiceNamespace;
+
+namespace SampleService.Service
 {
-    public class RootServiceClient
+    public interface IRootServiceClient
     {
+        public RootServiceNamespace.RootServiceClient RootServiceClient { get; }
+        public Task<IEnumerable<WeatherForecast>> Get();
+    }
+    public class RootServiceClient : IRootServiceClient
+    {
+        private readonly ILogger<RootServiceClient> _logger;
+        private readonly RootServiceNamespace.RootServiceClient _rootServiceClient;
+
+        RootServiceNamespace.RootServiceClient IRootServiceClient.RootServiceClient => _rootServiceClient;
+
+        public RootServiceClient(HttpClient httpClient, ILogger<RootServiceClient> logger)
+        {
+            _logger = logger;
+            _rootServiceClient = new RootServiceNamespace.RootServiceClient("http://localhost:5202", httpClient);
+        }
+
+        public async Task<IEnumerable<WeatherForecast>> Get()
+        {
+            return await _rootServiceClient.GetWeatherForecastAsync();
+        }
     }
 }
